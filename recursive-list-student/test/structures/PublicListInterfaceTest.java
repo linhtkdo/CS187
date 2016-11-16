379ae56f1ea280adee1170ed9399b4c5e7b695b9
Linh Do <linhdo@umass.edu>
2016-11-16 17:04:27 -0500

    Second Commit

---
+++ recursive-list-student/test/structures/PublicListInterfaceTest.java
@@ -0,0 +1,32 @@
+package structures;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class PublicListInterfaceTest {
+
+	private ListInterface<String> list;
+
+	@Before
+	public void setup(){
+          list = new RecursiveList<String>();
+	}
+
+	@Test (timeout = 500)
+	public void testInsertFirstIsEmptySizeAndGetFirst1() {
+		assertTrue("Newly constructed list should be empty.", list.isEmpty());
+		assertEquals("Newly constructed list should be size 0.", 0, list.size());
+		assertEquals("Insert First should return instance of self", list, list.insertFirst("hello"));
+		assertFalse("List should now have elements.", list.isEmpty());
+		assertEquals("List should now have 1 element.", 1, list.size());
+		assertEquals("First element should .equals \"hello\".", "hello", list.getFirst());
+		list.insertFirst("world");
+		assertEquals(2, list.size());
+		list.insertFirst("foo");
+		assertEquals(3, list.size());
+		assertEquals("First element should .equals \"foo\".", "foo", list.getFirst());
+	}
+}
